9b6d4ca8a7f069040c54b2757a323861cb62b8fb
Nikita <vclan@mail.ru>
2021-08-04 09:40:27 +0300

    Initial commit from Create Next App

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css';
+import '../styles/App.scss';
+import '../styles/Navigation.scss';
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
+
+export default MyApp;
